4880f6653053e13fb559542ab0096501076624e4
AlexeiSrour <96818193+AlexeiSrour@users.noreply.github.com>
2023-12-30 13:39:44 +0000

    Deploying to gh-pages from  @ ded0ccf20cbc279c7bb6172a1a751bda7e4a5b05 🚀

--- bundle/codedoc-bundle.entry.js
+++ bundle/codedoc-bundle.entry.js
@@ -1,36 +1,36 @@
-import { getRenderer } from '/home/runner/work/coding-blog-boilerplate/coding-blog-boilerplate/.codedoc/node_modules/@codedoc/core/dist/es6/transport/renderer.js';
-import { initJssCs } from '/home/runner/work/coding-blog-boilerplate/coding-blog-boilerplate/.codedoc/node_modules/@codedoc/core/dist/es6/transport/setup-jss.js';initJssCs();
-import { installTheme } from '/home/runner/work/coding-blog-boilerplate/coding-blog-boilerplate/.codedoc/content/theme.ts';installTheme();
-import { countCards } from '/home/runner/work/coding-blog-boilerplate/coding-blog-boilerplate/.codedoc/node_modules/@codedoc/coding-blog-plugin/dist/es5/components/article-card/count-cards.js';countCards();
-import { codeSelection } from '/home/runner/work/coding-blog-boilerplate/coding-blog-boilerplate/.codedoc/node_modules/@codedoc/core/dist/es6/components/code/selection.js';codeSelection();
-import { sameLineLengthInCodes } from '/home/runner/work/coding-blog-boilerplate/coding-blog-boilerplate/.codedoc/node_modules/@codedoc/core/dist/es6/components/code/same-line-length.js';sameLineLengthInCodes();
-import { initHintBox } from '/home/runner/work/coding-blog-boilerplate/coding-blog-boilerplate/.codedoc/node_modules/@codedoc/core/dist/es6/components/code/line-hint/index.js';initHintBox();
-import { initCodeLineRef } from '/home/runner/work/coding-blog-boilerplate/coding-blog-boilerplate/.codedoc/node_modules/@codedoc/core/dist/es6/components/code/line-ref/index.js';initCodeLineRef();
-import { initSmartCopy } from '/home/runner/work/coding-blog-boilerplate/coding-blog-boilerplate/.codedoc/node_modules/@codedoc/core/dist/es6/components/code/smart-copy.js';initSmartCopy();
-import { copyHeadings } from '/home/runner/work/coding-blog-boilerplate/coding-blog-boilerplate/.codedoc/node_modules/@codedoc/core/dist/es6/components/heading/copy-headings.js';copyHeadings();
-import { contentNavHighlight } from '/home/runner/work/coding-blog-boilerplate/coding-blog-boilerplate/.codedoc/node_modules/@codedoc/core/dist/es6/components/page/contentnav/highlight.js';contentNavHighlight();
-import { loadDeferredIFrames } from '/home/runner/work/coding-blog-boilerplate/coding-blog-boilerplate/.codedoc/node_modules/@codedoc/core/dist/es6/transport/deferred-iframe.js';loadDeferredIFrames();
-import { smoothLoading } from '/home/runner/work/coding-blog-boilerplate/coding-blog-boilerplate/.codedoc/node_modules/@codedoc/core/dist/es6/transport/smooth-loading.js';smoothLoading();
-import { tocHighlight } from '/home/runner/work/coding-blog-boilerplate/coding-blog-boilerplate/.codedoc/node_modules/@codedoc/core/dist/es6/components/page/toc/toc-highlight.js';tocHighlight();
-import { postNavSearch } from '/home/runner/work/coding-blog-boilerplate/coding-blog-boilerplate/.codedoc/node_modules/@codedoc/core/dist/es6/components/page/toc/search/post-nav/index.js';postNavSearch();
-import { copyLineLinks } from '/home/runner/work/coding-blog-boilerplate/coding-blog-boilerplate/.codedoc/node_modules/@codedoc/core/dist/es6/components/code/line-links/copy-line-link.js';copyLineLinks();
-import { gatherFootnotes } from '/home/runner/work/coding-blog-boilerplate/coding-blog-boilerplate/.codedoc/node_modules/@codedoc/core/dist/es6/components/footnote/gather-footnotes.js';gatherFootnotes();
-import { Author } from '/home/runner/work/coding-blog-boilerplate/coding-blog-boilerplate/.codedoc/node_modules/@codedoc/coding-blog-plugin/dist/es5/components/author/index.js';
-import { ToCToggle } from '/home/runner/work/coding-blog-boilerplate/coding-blog-boilerplate/.codedoc/node_modules/@codedoc/core/dist/es6/components/page/toc/toggle/index.js';
-import { DarkModeSwitch } from '/home/runner/work/coding-blog-boilerplate/coding-blog-boilerplate/.codedoc/node_modules/@codedoc/core/dist/es6/components/darkmode/index.js';
-import { ConfigTransport } from '/home/runner/work/coding-blog-boilerplate/coding-blog-boilerplate/.codedoc/node_modules/@codedoc/core/dist/es6/transport/config.js';
-import { ArticleCard } from '/home/runner/work/coding-blog-boilerplate/coding-blog-boilerplate/.codedoc/node_modules/@codedoc/coding-blog-plugin/dist/es5/components/article-card/index.js';
-import { ToCPrevNext } from '/home/runner/work/coding-blog-boilerplate/coding-blog-boilerplate/.codedoc/node_modules/@codedoc/core/dist/es6/components/page/toc/prevnext/index.js';
-import { TabSelector } from '/home/runner/work/coding-blog-boilerplate/coding-blog-boilerplate/.codedoc/node_modules/@codedoc/core/dist/es6/components/tabs/selector.js';
+import { getRenderer } from '/home/runner/work/blog/blog/.codedoc/node_modules/@codedoc/core/dist/es6/transport/renderer.js';
+import { initJssCs } from '/home/runner/work/blog/blog/.codedoc/node_modules/@codedoc/core/dist/es6/transport/setup-jss.js';initJssCs();
+import { installTheme } from '/home/runner/work/blog/blog/.codedoc/content/theme.ts';installTheme();
+import { countCards } from '/home/runner/work/blog/blog/.codedoc/node_modules/@codedoc/coding-blog-plugin/dist/es5/components/article-card/count-cards.js';countCards();
+import { codeSelection } from '/home/runner/work/blog/blog/.codedoc/node_modules/@codedoc/core/dist/es6/components/code/selection.js';codeSelection();
+import { sameLineLengthInCodes } from '/home/runner/work/blog/blog/.codedoc/node_modules/@codedoc/core/dist/es6/components/code/same-line-length.js';sameLineLengthInCodes();
+import { initHintBox } from '/home/runner/work/blog/blog/.codedoc/node_modules/@codedoc/core/dist/es6/components/code/line-hint/index.js';initHintBox();
+import { initCodeLineRef } from '/home/runner/work/blog/blog/.codedoc/node_modules/@codedoc/core/dist/es6/components/code/line-ref/index.js';initCodeLineRef();
+import { initSmartCopy } from '/home/runner/work/blog/blog/.codedoc/node_modules/@codedoc/core/dist/es6/components/code/smart-copy.js';initSmartCopy();
+import { copyHeadings } from '/home/runner/work/blog/blog/.codedoc/node_modules/@codedoc/core/dist/es6/components/heading/copy-headings.js';copyHeadings();
+import { contentNavHighlight } from '/home/runner/work/blog/blog/.codedoc/node_modules/@codedoc/core/dist/es6/components/page/contentnav/highlight.js';contentNavHighlight();
+import { loadDeferredIFrames } from '/home/runner/work/blog/blog/.codedoc/node_modules/@codedoc/core/dist/es6/transport/deferred-iframe.js';loadDeferredIFrames();
+import { smoothLoading } from '/home/runner/work/blog/blog/.codedoc/node_modules/@codedoc/core/dist/es6/transport/smooth-loading.js';smoothLoading();
+import { tocHighlight } from '/home/runner/work/blog/blog/.codedoc/node_modules/@codedoc/core/dist/es6/components/page/toc/toc-highlight.js';tocHighlight();
+import { postNavSearch } from '/home/runner/work/blog/blog/.codedoc/node_modules/@codedoc/core/dist/es6/components/page/toc/search/post-nav/index.js';postNavSearch();
+import { copyLineLinks } from '/home/runner/work/blog/blog/.codedoc/node_modules/@codedoc/core/dist/es6/components/code/line-links/copy-line-link.js';copyLineLinks();
+import { gatherFootnotes } from '/home/runner/work/blog/blog/.codedoc/node_modules/@codedoc/core/dist/es6/components/footnote/gather-footnotes.js';gatherFootnotes();
+import { Author } from '/home/runner/work/blog/blog/.codedoc/node_modules/@codedoc/coding-blog-plugin/dist/es5/components/author/index.js';
+import { ToCToggle } from '/home/runner/work/blog/blog/.codedoc/node_modules/@codedoc/core/dist/es6/components/page/toc/toggle/index.js';
+import { DarkModeSwitch } from '/home/runner/work/blog/blog/.codedoc/node_modules/@codedoc/core/dist/es6/components/darkmode/index.js';
+import { ConfigTransport } from '/home/runner/work/blog/blog/.codedoc/node_modules/@codedoc/core/dist/es6/transport/config.js';
+import { ArticleCard } from '/home/runner/work/blog/blog/.codedoc/node_modules/@codedoc/coding-blog-plugin/dist/es5/components/article-card/index.js';
+import { ToCPrevNext } from '/home/runner/work/blog/blog/.codedoc/node_modules/@codedoc/core/dist/es6/components/page/toc/prevnext/index.js';
+import { TabSelector } from '/home/runner/work/blog/blog/.codedoc/node_modules/@codedoc/core/dist/es6/components/tabs/selector.js';
 
 const components = {
-  'OK4ubc9p4Pms1u6h+OhSiQ==': Author,
-  'PsgqAhHaTlIADaHukhodzQ==': ToCToggle,
-  'rCJZP197GQZTK9359OVTXQ==': DarkModeSwitch,
-  'tV3VfOF15t1u5XdVsnT8EQ==': ConfigTransport,
-  'ZFIxsKD6IU/5ZuDhD6us8A==': ArticleCard,
-  'Dm8jHRiTvUujlM5pSk0bbg==': ToCPrevNext,
-  'owwldFhyta9Vx4AZPQrjyw==': TabSelector
+  'tvU4ZvTyVzKC9UotaYeV1w==': Author,
+  'k0lYj9deiYXngpZFqYY0vQ==': ToCToggle,
+  'QtGDrozMmpfzLYU0jY3n+w==': DarkModeSwitch,
+  'Rs0VnrLCFSwfOm1o3R6OHg==': ConfigTransport,
+  '4I2LlaAPZ3FDHhFl7YOveg==': ArticleCard,
+  'PFhwqFF68841RB6Nunq1Zg==': ToCPrevNext,
+  'vG6ZDf7oINlAp89F4hMUuA==': TabSelector
 };
 
 const renderer = getRenderer();
