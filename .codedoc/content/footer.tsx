65f84604a0940707257de9efbf989f06b257fa45
AlexeiSrour <alyosha85@hotmail.com>
2023-12-31 12:56:09 +1100

    added RSS, JSON, and ATOM feeds buttons to footer of most pages

--- .codedoc/content/footer.tsx
+++ .codedoc/content/footer.tsx
@@ -1,7 +1,18 @@
 import { CodedocConfig } from '@codedoc/core';
-import { Footer as _Footer } from '@codedoc/core/components';
+import { Footer as _Footer, Icon } from '@codedoc/core/components';
 
 
 export function Footer(config: CodedocConfig, renderer: any) {
-  return <_Footer/>;
+  return <_Footer>
+
+    <a href="https://alexeisrour.github.io/_feed.rss" target="_blank">
+	  <Icon>rss_feed</Icon> RSS
+    </a>
+    <a href="https://alexeisrour.github.io/_feed.json" target="_blank">
+	  <Icon>json_feed</Icon> JSON
+    </a>
+    <a href="https://alexeisrour.github.io/_feed.atom" target="_blank">
+	  <Icon>atom_feed</Icon> ATOM
+    </a>
+  </_Footer>;
 }
